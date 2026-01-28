dede5641a5a069b33215dfadd9a5dabe38aa217e
srushti <potdarsrushti4@gmail.com>
2026-01-28 21:46:19 +0530

    Updated URL of backend

--- frontend/src/services/weatherApi.js
+++ frontend/src/services/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://pazago-weather-chat-1.onrender.com";
 
 export const getWeather = async (city) => {
   const response = await axios.get(
